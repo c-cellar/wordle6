95d1600c3bcbecebc57ac70efd2106f652743e08
Christian <christian.l.keller@outlook.com>
2022-03-26 15:36:30 +0100

    Eingabe direkt in Array pushen

--- js/firstScript.js
+++ js/firstScript.js
@@ -5,8 +5,6 @@ const $$ = (q) => document.querySelectorAll(q)
 
 // abzugleichendes Wort
 let arrayGuess = [];
-let guess = arrayGuess;
-// let guess = '';
 
 // maximale Wortlänge
 const maxLength = 6;
@@ -29,9 +27,7 @@ const addKeyhandler = () => document.addEventListener('keydown', getKey);
 // === FUNCTIONS ====
 // TODO: löschen funktioniert nicht immer 
 // löscht letzte Eingabe
-const deleteLastLetter = (guess) => {
-    guess = guess.substring(0, guess.length - 1);
-    console.log(guess);
+const deleteLastLetter = () => {
     // Array ===========
     arrayGuess.pop();
     console.log(arrayGuess);
@@ -44,7 +40,7 @@ const getKey = (e) => {
     if (pattern.test(e.key) && (e.key.length < 2)) {
          
         // auf maximale Länge
-        if (guess.length < maxLength) {
+        if (arrayGuess.length < maxLength) {
             inputWord(e.key);
         };
     }
@@ -52,32 +48,26 @@ const getKey = (e) => {
     // auf "Backspace"-Eingabe
     if (e.key === "Backspace") {
         console.log(e.key)
-        deleteLastLetter(guess);
+        deleteLastLetter();
     };   
 
     // mit "Enter" und mindestlänge Vorhanden Wort auswerten
     // if (e.key === "Enter" && input.innerHTML.length == maxLength) {
-    if (e.key === "Enter" && guess.length == maxLength) {
+    if (e.key === "Enter" && arrayGuess.length == maxLength) {
         // guess = input.innerHTML;
-        console.log('Wort auswerten ' + guess);
-        checkLetter(word, guess);
+        console.log('Wort auswerten ' + arrayGuess);
+        checkLetter(word, arrayGuess);
     }
 };
 
 // erzeugt Ausgabe
 const inputWord = (letter) => {
-    // guess = guess + letter.toUpperCase();
-    // console.log(guess);
-
     // ================ Array
     arrayGuess.push(letter.toUpperCase());
     console.log(arrayGuess);
 }
 
-// output 
-const outputLetter = (letter) => {
-    
-}
+// TODO: output
 
 
 init();
