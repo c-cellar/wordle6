899ca5136e7b638fa3482ee88dfe1192dd8cb675
c-cellar <christian.l.keller@outlook.com>
2022-06-17 01:51:20 +0200

    created func for unsuccessful round

--- assets/js/arrayWords.js
+++ assets/js/arrayWords.js
@@ -3,9 +3,12 @@
 const arrayWords = ['result', 'target', 'flying', 'random'];
 const arrayPlayedWords = [];
 
-
 const getWordFromArray = (array) => {
-    const word = array.pop();
+    const word = array.shift();
     return word;
 };
 
+// wenn das Wort nicht erraten wurde, wird es wieder dem arrayWords hinzugefügt 
+const pushWordInArray = (word, array) => {
+    array.push(word);
+};
