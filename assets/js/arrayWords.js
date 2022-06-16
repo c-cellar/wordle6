85a39737f07ca3f0394e283ae1e47bb5f20a86f7
c-cellar <christian.l.keller@outlook.com>
2022-06-17 01:36:41 +0200

    implemented func to get word out of arryWords

--- assets/js/arrayWords.js
+++ assets/js/arrayWords.js
@@ -1,4 +1,11 @@
 'use strict';
-// Array with words 
-const arrayWords = ['result', 'target', 'flying', 'random']
+// Arrays with words and played words 
+const arrayWords = ['result', 'target', 'flying', 'random'];
+const arrayPlayedWords = [];
+
+
+const getWordFromArray = (array) => {
+    const word = array.pop();
+    return word;
+};
 
