c4459825c826608377ba97469b09e7cb7aae01ed
c-cellar <christian.l.keller@outlook.com>
2022-07-14 21:24:17 +0200

    changed formatting

--- assets/js/dictionary.js
+++ assets/js/dictionary.js
@@ -1,23 +1,22 @@
-'use strict';
-const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+"use strict";
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
-async function existingWord (arrayGuess) {
-    const guessWord = arrayGuess.join('');
+async function existingWord(arrayGuess) {
+  const guessWord = arrayGuess.join("");
 
-    await fetch(`${url}${guessWord}`) 
-        .then(response => {
-            console.log(response.status);
-            if (response.status === 200) {   
-                console.log('das wort gibt es!');
-                checkGuess(word, arrayGuess);
-                nextRound();
-            } else {
-                console.log('versuch es mal mit einem richtigen Wort');
-                blendInAndOutNoWordNotification(guessWord);
-            }})
-        .catch((err) => console.log(err));
-};
+  await fetch(`${url}${guessWord}`)
+    .then((response) => {
+      console.log(response.status);
+      if (response.status === 200) {
+        console.log("das wort gibt es!");
+        checkGuess(word, arrayGuess);
+        nextRound();
+      } else {
+        console.log("versuch es mal mit einem richtigen Wort");
+        blendInAndOutNoWordNotification(guessWord);
+      }
+    })
+    .catch((err) => console.log(err));
+}
 
 // TODO: API für deutsches Wörterbuch einbinden
-
-
