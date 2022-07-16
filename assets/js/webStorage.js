12280c646c50e5272424df874c43bdcb4b7e9a3b
c-cellar <christian.l.keller@outlook.com>
2022-07-16 13:57:37 +0200

    refactoring formatting

--- assets/js/webStorage.js
+++ assets/js/webStorage.js
@@ -1,40 +1,32 @@
-'use strict';
+"use strict";
 // use/set local storage for word array
 const setLocalStorageArrayWords = (array) => {
-    localStorage.setItem('unplayedWords', JSON.stringify(array));
-}
+  localStorage.setItem("unplayedWords", JSON.stringify(array));
+};
 
 // get word from local storage
 const getArrayFromLocalStorage = () => {
-    const myWordsFromStorage = JSON.parse(localStorage.getItem('unplayedWords'));
-    return myWordsFromStorage;
+  const myWordsFromStorage = JSON.parse(localStorage.getItem("unplayedWords"));
+  return myWordsFromStorage;
 };
 
-// use/set local storage for word array 
+// use/set local storage for word array
 const setLocalStorageTheme = () => {
-    // get the name of the used theme (dark or light)
-    const theme = html.classList
+  // get the name of the used theme (dark or light)
+  const theme = html.classList;
 
-    // check if a key like theme already exist
-    if (localStorage.getItem('theme')) {
-        // delete already existing key and value
-        localStorage.removeItem('theme');
-    }
+  // checks for a key "theme" in localStorage. if there is a key, remove item with key "theme"
+  localStorage.getItem("theme") ?? localStorage.removeItem("theme");
 
-    localStorage.setItem('theme', JSON.stringify(theme));
-}
-
-
-// get last used theme 
-const getLocalStorageTheme = () => {
-    const theme = JSON.parse(localStorage.getItem('theme')); //=> objekt
-
-    if (theme[0]) {
-        // remove the "light" class 
-        // before adding the theme class out of the storage
-        html.classList.remove('light');
-
-        html.classList.add(theme[0]);
-        // console.log('theme welches geladen werden muss: ' + theme[0]);
-    };
+  localStorage.setItem("theme", JSON.stringify(theme));
 };
+
+// get last used theme
+function getLocalStorageTheme() {
+  const themeObject = JSON.parse(localStorage.getItem("theme")); //=> objekt
+  //   console.log(themeObject);
+
+  themeObject === null
+    ? html.classList.add("light")
+    : html.classList.add(Object.values(themeObject));
+}
