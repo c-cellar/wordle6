c47017568517124b2714de0988fc96f5312f65e4
c-cellar <christian.l.keller@outlook.com>
2022-07-16 19:17:49 +0200

    added prettier.config.js to project

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+// Config-Datei für prettier. Ermöglicht es das alle Personen, die diese Datei einbinden die gleichen Einstellungen in prettier haben.
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+};
