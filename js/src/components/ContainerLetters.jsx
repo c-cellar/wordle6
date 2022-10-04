16375c5be8e3d9e9fc5391e107e1d2a153c4e3a0
c-cellar <christian.l.keller@outlook.com>
2022-10-04 13:46:50 +0200

    created & added ContainerLetter.jsx to ReactCompo.

---
+++ js/src/components/ContainerLetters.jsx
@@ -0,0 +1,54 @@
+export default function ContainerLetters() {
+  return (
+    <div className="letterContainer">
+      <div className="round" id="first">
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+      </div>
+      <div className="round" id="second">
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+      </div>
+      <div className="round" id="third">
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+      </div>
+      <div className="round" id="fourth">
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+      </div>
+      <div className="round" id="fifth">
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+      </div>
+      <div className="round" id="sixth">
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+        <div className="box-letter"></div>
+      </div>
+    </div>
+  );
+}
---
+++ js/src/components/ReactComponents.jsx
@@ -0,0 +1,11 @@
+import ContainerLetters from './ContainerLetters';
+import Keyboard from './Keyboard';
+
+export default function ReactComponents() {
+  return (
+    <div>
+      <ContainerLetters />
+      <Keyboard />
+    </div>
+  );
+}
