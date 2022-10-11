2bd090d6bd990bfa4aac1f17c52fa7bf5bf7ed23
c-cellar <christian.l.keller@outlook.com>
2022-10-11 16:33:40 +0200

    added output

--- js/src/components/ContainerLetters.jsx
+++ js/src/components/ContainerLetters.jsx
@@ -1,53 +1,57 @@
-export default function ContainerLetters({ arrayGuess }) {
+export default function ContainerLetters({ arrayGuess, checkedInput }) {
+  console.log('containerComponent');
+  console.log(arrayGuess);
+  console.log(checkedInput);
+
   return (
     <div className="letterContainer">
       <div className="round" id="first">
-        <div className="box-letter">{arrayGuess[0] ?? ''}</div>
-        <div className="box-letter">{arrayGuess[1] ?? ''}</div>
-        <div className="box-letter">{arrayGuess[2] ?? ''}</div>
-        <div className="box-letter">{arrayGuess[3] ?? ''}</div>
-        <div className="box-letter">{arrayGuess[4] ?? ''}</div>
-        <div className="box-letter">{arrayGuess[5] ?? ''}</div>
+        <div className="box-letter"> {arrayGuess[0] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[1] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[2] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[3] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[4] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[5] ?? ''} </div>
       </div>
       <div className="round" id="second">
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
+        <div className="box-letter"> {arrayGuess[0] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[1] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[2] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[3] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[4] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[5] ?? ''} </div>
       </div>
       <div className="round" id="third">
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
+        <div className="box-letter"> {arrayGuess[0] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[1] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[2] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[3] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[4] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[5] ?? ''} </div>
       </div>
       <div className="round" id="fourth">
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
+        <div className="box-letter"> {arrayGuess[0] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[1] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[2] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[3] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[4] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[5] ?? ''} </div>
       </div>
       <div className="round" id="fifth">
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
+        <div className="box-letter"> {arrayGuess[0] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[1] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[2] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[3] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[4] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[5] ?? ''} </div>
       </div>
       <div className="round" id="sixth">
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
-        <div className="box-letter"></div>
+        <div className="box-letter"> {arrayGuess[0] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[1] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[2] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[3] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[4] ?? ''} </div>
+        <div className="box-letter"> {arrayGuess[5] ?? ''} </div>
       </div>
     </div>
   );
