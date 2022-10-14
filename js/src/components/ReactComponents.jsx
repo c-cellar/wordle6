9efb8e486cab5691650dadbb9db6eb1d5186ff5f
c-cellar <christian.l.keller@outlook.com>
2022-10-14 17:23:37 +0200

    implemented states for output

--- js/src/components/ReactComponents.jsx
+++ js/src/components/ReactComponents.jsx
@@ -10,8 +10,20 @@ export default function ReactComponents() {
   const [searchedWord, setSearchedWord] = useState('');
   console.log(searchedWord);
 
-  //-------- Custom Hook ----------------
-  const { arrayGuess, dispatchArrayGuess, checkedInput } = useProcessInput();
+  //-------- Custom Hook ------------- (return)
+  const {
+    arrayGuess,
+    dispatchArrayGuess,
+    currentRound,
+    firstRound,
+    secondRound,
+    thirdRound,
+    fourthRound,
+    fifthRound,
+    sixthRound,
+    checkedInput,
+    setCheckedInput,
+  } = useProcessInput();
 
   // keyHandler on document für die Eingabe mit der Tastatur
   useEffect(() => {
@@ -24,7 +36,18 @@ export default function ReactComponents() {
 
   return (
     <div>
-      <ContainerLetters arrayGuess={arrayGuess} checkedInput={checkedInput} />
+      <ContainerLetters
+        arrayGuess={arrayGuess}
+        currentRound={currentRound}
+        firstRound={firstRound}
+        secondRound={secondRound}
+        thirdRound={thirdRound}
+        fourthRound={fourthRound}
+        fifthRound={fifthRound}
+        sixthRound={sixthRound}
+        checkedInput={checkedInput}
+        setCheckedInput={setCheckedInput}
+      />
       <Keyboard dispatchArrayGuess={dispatchArrayGuess} />
     </div>
   );
