9b62937f048a8e55603b9b5f47b16ab071d3f3e6
c-cellar <christian.l.keller@outlook.com>
2022-10-20 18:30:06 +0200

    created component

---
+++ js/src/components/NoWordNotification.jsx
@@ -0,0 +1,8 @@
+export default function NoWordNotification() {
+  return (
+    <div id="noWord-notification" className="notification">
+      <span id="noWord"></span>
+      <span>is not in used dictionary</span>
+    </div>
+  );
+}
