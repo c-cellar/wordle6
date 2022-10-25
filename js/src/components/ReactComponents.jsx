32f86cbb9e6ece9779a6723ac00cad8881f770af
c-cellar <christian.l.keller@outlook.com>
2022-10-25 16:48:36 +0200

    implemented function to add dark-theme-layout

--- js/src/components/ReactComponents.jsx
+++ js/src/components/ReactComponents.jsx
@@ -33,7 +33,8 @@ export default function ReactComponents() {
 
     setSearchedWord(arrayWords[0]);
 
-    getSelectedTheme();
+    const selectedTheme = getSelectedTheme();
+    addClassToElementsFor(selectedTheme);
   }, []);
 
   return (
@@ -59,9 +60,40 @@ export default function ReactComponents() {
 
 // checks which theme is/was selected
 function getSelectedTheme() {
-  document.documentElement.classList.length === 0
-    ? document.documentElement.classList.add('light')
-    : '';
+  if (document.documentElement.classList.length === 0) {
+    document.documentElement.classList.add('light');
+    return document.documentElement.classList.value;
+  } else {
+    console.log(document.documentElement.classList.value);
+    return document.documentElement.classList.value;
+  }
+}
+
+// add classes for dark-theme and removed them for light-theme
+function addClassToElementsFor(selectedTheme) {
+  switch (selectedTheme) {
+    case 'dark':
+      // "headline" gets the dark-theme class
+      document.querySelectorAll('h1 > span').forEach((el) => {
+        el.classList.add('header-dark');
+        el.classList.add('boxLetter-dark');
+      });
+      // boxletters gets the dark-theme class
+      document.querySelectorAll('.boxLetter').forEach((el) => {
+        el.classList.add('boxLetter-dark');
+      });
+
+      // keys get the dark-theme class
+      document.querySelectorAll('.key').forEach((el) => {
+        el.classList.add('boxLetter-dark');
+      });
+      break;
+    case 'light':
+      document.querySelectorAll('h1 > span').forEach((el) => {
+        el.classList.remove('box-letter-dark');
+        el.classList.remove('header-dark');
+      });
+  }
 }
 
 // TODO: ausgewähltes Theme im Browser speichern und beim erneuten Laden der Seite auslesen
