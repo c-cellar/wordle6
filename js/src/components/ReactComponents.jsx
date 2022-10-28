8a0373d52923ab218b6e4c0a127bae41c2654de5
c-cellar <christian.l.keller@outlook.com>
2022-10-28 15:02:47 +0200

    implemented theme selection

--- js/src/components/ReactComponents.jsx
+++ js/src/components/ReactComponents.jsx
@@ -1,17 +1,22 @@
 import arrayWords from '../../arrayWords';
 import { openMenu } from '../../burgerMenuFunction';
+import {
+  addClassToElementsFor,
+  getThemeFromStorage,
+} from '../../themeSelection';
 
 // Hooks
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useProcessInput } from '../hooks/useProcessInput';
 
 // Components
+import Header from './Header';
 import ContainerLetters from './ContainerLetters';
 import Keyboard from './Keyboard';
 import Notifications from './Notifications';
-import Header from './Header';
 
 export default function ReactComponents() {
+  const [colorTheme, setColorTheme] = useState(getThemeFromStorage());
   //-------- Custom Hook(Rückgaben) -------------
   const {
     searchedWord,
@@ -33,19 +38,21 @@ export default function ReactComponents() {
       dispatchArrayGuess({ inputType: e.type, input: e.key });
     });
 
-    setSearchedWord(arrayWords[0]);
-
     document
       .querySelector('.hamburger-menu-icon')
       .addEventListener('click', () => openMenu());
 
-    const selectedTheme = getSelectedTheme();
-    addClassToElementsFor(selectedTheme);
+    // nimmt ein Wort aus dem Array
+    setSearchedWord(arrayWords[0]);
+
+    // zuweisen des Themes beim ersten Laden der Application
+    getThemeFromStorage();
+    addClassToElementsFor(colorTheme);
   }, []);
 
   return (
     <div>
-      <Header />
+      <Header colorTheme={colorTheme} setColorTheme={setColorTheme} />
       <ContainerLetters
         arrayGuess={arrayGuess}
         currentRound={currentRound}
@@ -64,43 +71,3 @@ export default function ReactComponents() {
     </div>
   );
 }
-
-// checks which theme is/was selected
-function getSelectedTheme() {
-  if (document.documentElement.classList.length === 0) {
-    document.documentElement.classList.add('light');
-    return document.documentElement.classList.value;
-  } else {
-    console.log(document.documentElement.classList.value);
-    return document.documentElement.classList.value;
-  }
-}
-
-// add classes for dark-theme and removed them for light-theme
-function addClassToElementsFor(selectedTheme) {
-  switch (selectedTheme) {
-    case 'dark':
-      // "headline" gets the dark-theme class
-      document.querySelectorAll('h1 > span').forEach((el) => {
-        el.classList.add('header-dark');
-        el.classList.add('boxLetter-dark');
-      });
-      // boxletters gets the dark-theme class
-      document.querySelectorAll('.boxLetter').forEach((el) => {
-        el.classList.add('boxLetter-dark');
-      });
-
-      // keys get the dark-theme class
-      document.querySelectorAll('.key').forEach((el) => {
-        el.classList.add('boxLetter-dark');
-      });
-      break;
-    case 'light':
-      document.querySelectorAll('h1 > span').forEach((el) => {
-        el.classList.remove('box-letter-dark');
-        el.classList.remove('header-dark');
-      });
-  }
-}
-
-// TODO: ausgewähltes Theme im Browser speichern und beim erneuten Laden der Seite auslesen
